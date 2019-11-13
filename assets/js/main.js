d75ec3c78b458bb9390fd5bbb578ae4e41a93126
imeda <imedabeqaia@gmail.com>
2019-11-13 19:44:43 +0400

    bottom language bar edit
    bottom language bar edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,8 @@ $( document ).ready(function() {
         $(".footer-content").toggleClass("active");
     })
 
-
+    $("#video")[0].autoplay = true;
+    $("#video")[0].play();
+    $("#video").attr('autoplay', true);
+    $("#video").trigger('play');
 });
